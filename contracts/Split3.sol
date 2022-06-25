7ea4cf76be36d9d815a16879e90a7229990ee38a
Chris Smith <chris@smith.xyz>
2022-06-25 20:25:04 +0000

    Transaction splitting

--- contracts/Split3.sol
+++ contracts/Split3.sol
@@ -4,14 +4,21 @@ pragma solidity >=0.8.0 <0.9.0;
 contract Split3 {
     mapping(address => int) public balances;
 
+    // Deposit into the contract to enable paying ahead on your debt. Also enables others to take loans
     function deposit() public payable {
         balances[msg.sender] += int(msg.value);
     }
 
+    // Take loans, pay back with deposit()
     function withdraw(uint amount) public {
         require (amount <= address(this).balance, "Contract does not have enough funds to loan");
         balances[msg.sender] -= int(amount);
         (bool sent, ) = msg.sender.call{value: amount}("");
         require(sent, "Failed to send");
+    }
+
+    // Track debt transactions
+    function adjust(address addr, int amount) public {
+        balances[addr] += amount;
     }
 }
--- test/test.js
+++ test/test.js
@@ -2,6 +2,8 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 const zero = ethers.utils.parseEther("0");
+const one = ethers.utils.parseEther("1.0");
+const negOne = ethers.utils.parseEther("-1.0")
 
 async function address() {
     const signer = await ethers.getSigner();
@@ -16,18 +18,28 @@ async function deploy() {
 }
 
 describe("Split3", () => {
+    it("Split transaction & pay back", async () => {
+        const split3 = await deploy();
+
+        await (await split3.adjust(await address(), one)).wait();
+
+        expect(await split3.balances(await address())).to.equal(one);
+        expect(await ethers.provider.getBalance(split3.address)).to.equal(zero);
+
+        await (await split3.adjust(await address(), negOne)).wait();
+        expect(await split3.balances(await address())).to.equal(zero);
+        expect(await ethers.provider.getBalance(split3.address)).to.equal(zero);
+    });
+
     it("Deposit & Withdraw", async () => {
         const split3 = await deploy();
 
-        const value = ethers.utils.parseEther("1.0");
-        const deposit = await split3.deposit({ value });
-        await deposit.wait();
+        await (await split3.deposit({ value: one })).wait();
 
-        expect(await split3.balances(await address())).to.equal(value);
-        expect(await ethers.provider.getBalance(split3.address)).to.equal(value);
+        expect(await split3.balances(await address())).to.equal(one);
+        expect(await ethers.provider.getBalance(split3.address)).to.equal(one);
 
-        const tx = await split3.withdraw(value);
-        await tx.wait();
+        await (await split3.withdraw(one)).wait();
 
         expect(await split3.balances(await address())).to.equal(zero);
         expect(await ethers.provider.getBalance(split3.address)).to.equal(zero);
