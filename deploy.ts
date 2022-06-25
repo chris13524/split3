0607fde27d536aacab32e722561c31be9774aa57
Chris Smith <chris@smith.xyz>
2022-06-25 19:10:10 +0000

    Initial contract

---
+++ deploy.ts
@@ -0,0 +1,17 @@
+import { ethers } from "hardhat";
+
+async function main() {
+  const Split3 = await ethers.getContractFactory("Split3");
+  const split3 = await Split3.deploy();
+
+  await split3.deployed();
+
+  console.log("Split3 deployed to:", split3.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
